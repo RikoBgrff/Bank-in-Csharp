bd866664ea25d2d342ef007f2b5a9b27c3acd6a6
Bagirli Arifali Azad <Bagi_mz73@itstep.edu.az>
2021-02-09 17:49:04 +0400

    Add project files.

---
+++ ConsoleApp4/Operations in Bank/Credit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Credit:Instance
+    {
+        public Client Client { get; set; }
+        public double Amount { get; set; }
+        public double Percent { get; set; }
+
+        public int Months { get; set; }
+        public double Payment { get; set; }
+        public double PaymentPerMonth { get; set; }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp4
+{
+   
+    class Instance
+    {
+        public Guid ID { get; set; }= Guid.NewGuid();
+    }
+    class Human: Instance
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+        public string Position { get; set; }
+        
+    }
+    interface IOrganize
+    {
+        
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadLine();
+        }
+    }
+}
